090841a9eab9cc78baede6f2eba4df40027fa846
Narayan Deshmukh <narayandeshmukh16@gmail.com>
2025-01-18 14:20:56 +0530

    feat: add website layout and placeholder content

---
+++ src/constants/static-data.ts
@@ -0,0 +1,24 @@
+const fullData = {
+    name: 'Narayan Deshmukh',
+    currentOrg: {
+        name: 'Springer Nature',
+        link: 'https://www.springernature.com/gp',
+        logo: ''
+    },
+    experience: [
+        {
+            org: 'ABC',
+            start: '',
+            end: ''
+        }
+    ],
+    education: [
+        {
+            org: 'PQR',
+            start: '',
+            end: ''
+        }
+    ]
+}
+
+export default fullData;
