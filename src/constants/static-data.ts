d7174c1358baa1833017201b96a5ef97260aa96e
Narayan Deshmukh <narayandeshmukh16@gmail.com>
2025-01-18 19:12:49 +0530

    docs: update placeholder content with actual profile data

--- src/constants/static-data.ts
+++ src/constants/static-data.ts
@@ -6,19 +6,71 @@ const fullData = {
         logo: ''
     },
     experience: [
+        {   title: 'Frontend Engineer',
+            org: 'Springer Nature Group',
+            startDate: 'Jan 2024',
+            endDate: 'Present'
+        },
         {
-            org: 'ABC',
-            start: '',
-            end: ''
+            title: 'Frontend Engineer',
+            org: 'Urban Technology GmBH',
+            startDate: 'Oct 2022',
+            endDate: 'Nov 2023'
+        },
+        {
+            title: 'Frontend Engineer',
+            org: 'KPS Software GmBH',
+            startDate: 'Jun 2022',
+            endDate: 'Sept 2022'
+        },
+        {
+            title: 'Senior Software Engineer - UI',
+            org: 'Cornerstone Ondemand',
+            startDate: 'May 2021',
+            endDate: 'Apr 2022'
+        },
+        {
+            title: 'Senior Software Engineer',
+            org: 'CitiusTech',
+            startDate: 'Mar 2020',
+            endDate: 'May 2021'
+        },
+        {
+            title: 'Assistant System Engineer',
+            org: 'Tata Consultancy Services',
+            startDate: 'Jun 2018',
+            endDate: 'Mar 2020'
         }
     ],
     education: [
+        {   title: `Bachelor's in Computer Engineering`,
+            org: 'University of Mumbai',
+            startDate: 'Aug 2014',
+            endDate: 'Jul 2018'
+        }
+    ],
+    contact: [
         {
-            org: 'PQR',
-            start: '',
-            end: ''
+            name: 'LinkedIn',
+            logo: '',
+            link: 'https://linkedin.com/in/narayand16'
+        },
+        {
+            name: 'Github',
+            logo: '',
+            link: 'https://github.com/narayand16'
+        },
+        {
+            name: 'X / Twitter',
+            logo: '',
+            link: 'https://twitter.com/narayand16_'
+        },
+        {
+            name: 'Email',
+            link: 'mailto:narayan.deshmukh0607@gmail.com'
         }
     ]
+
 }
 
 export default fullData;
